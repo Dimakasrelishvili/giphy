147ba9a24c2d0cea4b4b96bff17373a53a62900c
Dimakasrelishvili <50391231+Dimakasrelishvili@users.noreply.github.com>
2019-08-01 17:56:49 -0400

    giphy

---
+++ js/ess.js
@@ -0,0 +1,22 @@
+M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92
+
+
+
+
+M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92
+
+
+var dogs= ""
+
+
+//javascript, jQuery
+
+var queryURL = ("http://api.giphy.com/v1/gifs/search?q="+dogs+"&api_key=M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92&limit=5");
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+
+console.log(" Goes great on.... nothing.");
+if(process.argv[2]==process.argv[3]){console.log(true)}
+else{console.log("falseee")}
+console.log(process.argv)
+
+if(process.argv[2]=="divide"){console.log(parseInt(process.argv[3])/parseInt(process.argv[4]))}
+if(process.argv[2]=="multiple"){console.log(parseInt(process.argv[3])*parseInt(process.argv[4]))}
+if(process.argv[2]=="plus"){console.log(parseInt(process.argv[3])+parseInt(process.argv[4]))} 
+
+
+console.log(process.argv[2]%7==0) 
+
+
+
+
+var stuff = require("./ess.js");cc 
+
+
+
+
