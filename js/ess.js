cbd8a0503228e65ba7e22f6519426d713ca37c43
Dimakasrelishvili <50391231+Dimakasrelishvili@users.noreply.github.com>
2020-04-16 05:39:56 -0400

    button

--- js/ess.js
+++ js/ess.js
@@ -1,18 +1,54 @@
 //M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92
+$(document).ready(function(){
+
+var topics= ["dog","cat","tiger","mouse","squirel"]
+
+ for(i=0; i<topics.length; i++){
+ var topButton = $("<button>")
+ topButton.text(topics[i])
+ topButton.attr({"value":topics[i], "id":"button"+i, "class":"button"})
+ $("#gifHolder").append(topButton)
+ console.log(topButton.attr("class"))
+ };
 
 
+ 
+ $(".button").on("click", function(){
+  
+  var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" +$(this).attr("value")+"&api_key=M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92&limit=5");
 
- var dogs= "dog"
-
-
-//javascript, jQuery
-
-var queryURL = ("http://api.giphy.com/v1/gifs/search?q="+dogs+"&api_key=M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92&limit=5");
-
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
+ $.ajax({
+   url: queryURL,
+   method: "GET"
   }).then(function(response) {
-    console.log(response);
-  });
+    console.log(response.data[0]);
+    
+  });  
+ 
+  
+  
+});
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+})
