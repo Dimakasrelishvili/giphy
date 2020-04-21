2eb3c01d76df6bda005fab35009acfbb2339a9b2
Dimakasrelishvili <50391231+Dimakasrelishvili@users.noreply.github.com>
2020-04-20 23:25:14 -0400

    dynamicly adding buttons, make on click ajax calls, inject elements in the window

--- js/ess.js
+++ js/ess.js
@@ -1,33 +1,64 @@
 //M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92
 $(document).ready(function(){
+  
 
-var topics= ["dog","cat","tiger","mouse","squirel"]
+  var topics= ["dog","cat","tiger","mouse","squirel","bear"]
+  topicArray();
+  function resetButtons(){
+    $("#gifff").empty();
+  }
 
+  function topicArray() {
+  
+   
  for(i=0; i<topics.length; i++){
  var topButton = $("<button>")
  topButton.text(topics[i])
  topButton.attr({"value":topics[i], "id":"button"+i, "class":"button"})
- $("#gifHolder").append(topButton)
+ $("#gifff").append(topButton)
  console.log(topButton.attr("class"))
+ 
  };
+}
+ 
+  
+  $("#submit").on("click",function(){
+    resetButtons()
+    
+    topics.push($("#fname").val().trim())
+    
+    
+    console.log(topics)
+    topicArray()
+    
+  })
+
 
 
  
- $(".button").on("click", function(){
+ $("body").delegate(".button", "click", function(){
+   console.log("clicked")
   
-  var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" +$(this).attr("value")+"&api_key=M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92&limit=5");
+  var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" +$(this).attr("value")+"&api_key=M1TfEu4vJtgbPsXFdBBz1KFdOFVhEP92&limit=10");
 
  $.ajax({
    url: queryURL,
    method: "GET"
   }).then(function(response) {
-    console.log(response.data[0]);
-    
+    var result = response.data
+   for (i=0; i<result.length; i++){
+     var forGif = $("<img>")
+     forGif.attr({"class":"gif", "id":"gif"+i,"src":result[i].images.original.url})
+     $("#gifHolder").append(forGif)
+
+   }
+   
+  console.log(result)
+  
   });  
+ });
+
  
-  
-  
-});
     
    
     
